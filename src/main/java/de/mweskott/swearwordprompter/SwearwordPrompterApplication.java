93a2c96baaf37ef3129b32bc1f5adcf5615ef82b
Markus Weskott <Martt@brain-force.intern>
2021-02-07 15:51:38 +0100

    spring initializr

---
+++ src/main/java/de/mweskott/swearwordprompter/SwearwordPrompterApplication.java
@@ -0,0 +1,13 @@
+package de.mweskott.swearwordprompter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwearwordPrompterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwearwordPrompterApplication.class, args);
+	}
+
+}
